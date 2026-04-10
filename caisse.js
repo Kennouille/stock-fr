39d6bfd3460b8fd5f686aff8f6db98995188f914
Kennouille <muhlemannk@gmail.com>
2026-04-09 22:48:08 -0600

    Amélioration 48

--- caisse.js
+++ caisse.js
@@ -9,7 +9,7 @@ let amountGiven = 0;
 let pendingArticle = null;
 let lastSaleData = null;
 
-const denominations = [200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
+const denominations = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
 const counts = {};
 denominations.forEach(d => counts[d] = 0);
 
